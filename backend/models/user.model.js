e0044fc23d650c7091636c2db6a96c996bf6fc4e
parzival29sundar <utkarshsundar18poprock@gmail.com>
2024-02-22 20:50:21 +0530

    first part done -backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8
+    },
+    gender:{
+        type:String,
+        required:true,
+        enum: ["male", "female"],
+    },
+    profilePic:{
+        type:String,
+        default:""
+    },
+},{timestamps:true}); //this timestamps will also record the schema creation time of update and create into MongoDB
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User; 
